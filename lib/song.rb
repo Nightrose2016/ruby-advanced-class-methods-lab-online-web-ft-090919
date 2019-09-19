48bd97dbccd2740dbc22d7eb03ad7120b94aec03
Lialah Barrer <lrbarrer@gmail.com>
2019-09-19 22:54:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,5 +32,33 @@ class Song
     self.all.detect { |song| song.name == sname}
   end
   
+    def self.find_or_create_by_name(sname)
+    self.find_by_name(sname) || self.create_by_name(sname)
+  end
   
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+  
+  def self.new_from_filename(fname)
+    data = fname.split(" - ")
+    song_and_format = data[1].split(".")
+    song = self.new
+    song.name = song_and_format[0]
+    song.artist_name = data[0]
+    song
+  end
+  
+  def self.create_from_filename(fname)
+    data = fname.split(" - ")
+    song_and_format = data[1].split(".")
+    song = self.create
+    song.name = song_and_format[0]
+    song.artist_name = data[0]
+    song
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 end
